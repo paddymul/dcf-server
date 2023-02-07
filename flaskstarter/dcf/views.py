bb5bbe4b7f6407c7ae7736adf2986d44e4ddd860
Paddy Mullen <paddy@paddymullen.com>
2023-02-07 11:54:16 -0500

    serving a hardcoded dataframe

--- flaskstarter/dcf/views.py
+++ flaskstarter/dcf/views.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
 import pandas as pd
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
 from ..extensions import db
-
-
-#from .forms import MyTaskForm
-#from .models import MyTaskModel
-
+import json
 
 dcf_views = Blueprint('dcf', __name__, url_prefix='/dcf')
 
@@ -17,84 +13,8 @@ dcf_views = Blueprint('dcf', __name__, url_prefix='/dcf')
 def read_df(id):
     
     print(os.getcwd())
-    return "paddy"
+    "/Users/paddy/code/dcf-server"
+    df = pd.read_csv('./flaskstarter/dcf/sample-data/2014-01-citibike-tripdata.csv')
+    return json.loads(df.to_json(orient='table', indent=2))
+    #return os.getcwd()
 
-
-
-# @tasks.route('/view_task/<id>', methods=['GET', 'POST'])
-# @login_required
-# def view_task(id):
-#     _task = MyTaskModel.query.filter_by(id=id, users_id=current_user.id).first()
-
-#     if not _task:
-#         flash('Oops! Something went wrong!.', 'danger')
-#         return redirect(url_for("tasks.my_tasks"))
-
-#     return render_template('tasks/view_task.html',
-#                            task=_task)
-
-
-# @tasks.route('/add_task', methods=['GET', 'POST'])
-# @login_required
-# def add_task():
-
-#     _task = MyTaskModel()
-
-#     _form = MyTaskForm()
-
-#     if _form.validate_on_submit():
-
-#         _task.users_id = current_user.id
-
-#         _form.populate_obj(_task)
-
-#         db.session.add(_task)
-#         db.session.commit()
-
-#         db.session.refresh(_task)
-#         flash('Your task is added successfully!', 'success')
-#         return redirect(url_for("tasks.my_tasks"))
-
-#     return render_template('tasks/add_task.html', form=_form, _active_tasks=True)
-
-
-# @tasks.route('/delete_task/<id>', methods=['GET', 'POST'])
-# @login_required
-# def delete_task(id):
-#     _task = MyTaskModel.query.filter_by(id=id, users_id=current_user.id).first()
-
-#     if not _task:
-#         flash('Oops! Something went wrong!.', 'danger')
-#         return redirect(url_for("tasks.my_tasks"))
-
-#     db.session.delete(_task)
-#     db.session.commit()
-
-#     flash('Your task is deleted successfully!', 'success')
-#     return redirect(url_for('tasks.my_tasks'))
-
-
-# @tasks.route('/edit_task/<id>', methods=['GET', 'POST'])
-# @login_required
-# def edit_task(id):
-#     _task = MyTaskModel.query.filter_by(id=id, users_id=current_user.id).first()
-
-#     if not _task:
-#         flash('Oops! Something went wrong!.', 'danger')
-#         return redirect(url_for("tasks.my_tasks"))
-
-#     _form = MyTaskForm(obj=_task)
-
-#     if _form.validate_on_submit():
-
-#         _task.users_id = current_user.id
-#         _form.populate_obj(_task)
-
-#         db.session.add(_task)
-#         db.session.commit()
-
-#         flash('Your task updated successfully!', 'success')
-#         return redirect(url_for("tasks.my_tasks"))
-
-#     return render_template('tasks/edit_task.html', form=_form, task=_task, _active_tasks=True)
-# -                               
