9f2299ababf0a1f1e03fa78e976c19acf256fdee
Paddy Mullen <paddy@paddymullen.com>
2023-02-14 12:26:01 -0500

    WIP

--- flaskstarter/dcf/lispy.py
+++ flaskstarter/dcf/lispy.py
@@ -205,11 +205,91 @@ def add_globals(env):
     })
     return env
 
-def make_interpreter(extra_funcs=None):
+def expand(x, macro_table, builtins, toplevel=False, ):
+    "Walk tree of x, making optimizations/fixes, and signaling SyntaxError."
+
+    _set, _if, _definemacro = builtins['_set'], builtins['_if'], builtins['_definemacro']
+    _lambda, _quasiquote = builtins['_lambda'], builtins['_quasiquote']
+
+    def local_expand(_x, _toplevel=False):
+        return expand(_x, macro_table, builtins, _toplevel)
+    require(x, x!=[])                    # () => Error
+    if not isa(x, list):                 # constant => unchanged
+        return x
+    elif x[0] is _quote:                 # (quote exp)
+        require(x, len(x)==2)
+        return x
+    elif x[0] is _if:                    
+        if len(x)==3: x = x + [None]     # (if t c) => (if t c None)
+        require(x, len(x)==4)
+        return [y for y in map(local_expand, x)]
+    elif x[0] is _set:                   
+        require(x, len(x)==3); 
+        var = x[1]                       # (set! non-var exp) => Error
+        require(x, isa(var, Symbol), "can set! only a symbol")
+        return [_set, var, expand(x[2])]
+    elif x[0] is _define or x[0] is _definemacro: 
+        require(x, len(x)>=3)            
+        _def, v, body = x[0], x[1], x[2:]
+        if isa(v, list) and v:           # (define (f args) body)
+            f, args = v[0], v[1:]        #  => (define f (lambda (args) body))
+            return expand([_def, f, [_lambda, args]+body])
+        else:
+            require(x, len(x)==3)        # (define non-var/list exp) => Error
+            require(x, isa(v, Symbol), "can define only a symbol")
+            exp = expand(x[2])
+            if _def is _definemacro:     
+                require(x, toplevel, "define-macro only allowed at top level")
+                proc = eval(exp)       
+                require(x, callable(proc), "macro must be a procedure")
+                macro_table[v] = proc    # (define-macro v proc)
+                return None              #  => None; add v:proc to macro_table
+            return [_define, v, exp]
+    elif x[0] is _begin:
+        if len(x)==1: return None        # (begin) => None
+        else: return [expand(xi, toplevel) for xi in x]
+    elif x[0] is _lambda:                # (lambda (x) e1 e2) 
+        require(x, len(x)>=3)            #  => (lambda (x) (begin e1 e2))
+        vars, body = x[1], x[2:]
+        require(x, (isa(vars, list) and all(isa(v, Symbol) for v in vars))
+                or isa(vars, Symbol), "illegal lambda argument list")
+        exp = body[0] if len(body) == 1 else [_begin] + body
+        return [_lambda, vars, local_expand(exp)]   
+    elif x[0] is _quasiquote:            # `x => expand_quasiquote(x)
+        require(x, len(x)==2)
+        return expand_quasiquote(x[1], builtins)
+    elif isa(x[0], Symbol) and x[0] in macro_table:
+        return local_expand(macro_table[x[0]](*x[1:])) # (m arg...) 
+    else:                                #        => macroexpand if m isa macro
+        return [y for y in map(local_expand, x)]            # (f arg...) => expand each
+
+
+def expand_quasiquote(x, builtins):
+    """Expand `x => 'x; `,x => x; `(,@x y) => (append x y) """
+
+    _quote, _unquotesplicing = builtins['_quote'], builtins['_unquotesplicing']
+    _unquote, _cons, _append = builtins['_unquote'], builtins['_cons'], builtins['_append']
+    if not is_pair(x):
+        return [_quote, x]
+    require(x, x[0] is not _unquotesplicing, "can't splice here")
+    if x[0] is _unquote:
+        require(x, len(x)==2)
+        return x[1]
+    elif is_pair(x[0]) and x[0][0] is _unquotesplicing:
+        require(x[0], len(x[0])==2)
+        return [_append, x[0][1], expand_quasiquote(x[1:])]
+    else:
+        return [_cons, expand_quasiquote(x[0]), expand_quasiquote(x[1:])]
+
+
+
+def make_interpreter(extra_funcs=None, extra_macros=None):
     if extra_funcs is None:
         ef = {}
     else:
         ef = extra_funcs
+    if extra_macros is None:
+        base_macros
 
     local_env = add_globals(Env())
     local_env.update(ef)
@@ -287,63 +367,6 @@ def make_interpreter(extra_funcs=None):
         return symbol_table[s]
     
 ################ expand
-
-
-    def expand(x, toplevel=False):
-        "Walk tree of x, making optimizations/fixes, and signaling SyntaxError."
-        require(x, x!=[])                    # () => Error
-        if not isa(x, list):                 # constant => unchanged
-            return x
-        elif x[0] is _quote:                 # (quote exp)
-            require(x, len(x)==2)
-            return x
-        elif x[0] is _if:                    
-            if len(x)==3: x = x + [None]     # (if t c) => (if t c None)
-            require(x, len(x)==4)
-            return [y for y in map(expand, x)]
-        elif x[0] is _set:                   
-            require(x, len(x)==3); 
-            var = x[1]                       # (set! non-var exp) => Error
-            require(x, isa(var, Symbol), "can set! only a symbol")
-            return [_set, var, expand(x[2])]
-        elif x[0] is _define or x[0] is _definemacro: 
-            require(x, len(x)>=3)            
-            _def, v, body = x[0], x[1], x[2:]
-            if isa(v, list) and v:           # (define (f args) body)
-                f, args = v[0], v[1:]        #  => (define f (lambda (args) body))
-                return expand([_def, f, [_lambda, args]+body])
-            else:
-                require(x, len(x)==3)        # (define non-var/list exp) => Error
-                require(x, isa(v, Symbol), "can define only a symbol")
-                exp = expand(x[2])
-                if _def is _definemacro:     
-                    require(x, toplevel, "define-macro only allowed at top level")
-                    proc = eval(exp)       
-                    require(x, callable(proc), "macro must be a procedure")
-                    macro_table[v] = proc    # (define-macro v proc)
-                    return None              #  => None; add v:proc to macro_table
-                return [_define, v, exp]
-        elif x[0] is _begin:
-            if len(x)==1: return None        # (begin) => None
-            else: return [expand(xi, toplevel) for xi in x]
-        elif x[0] is _lambda:                # (lambda (x) e1 e2) 
-            require(x, len(x)>=3)            #  => (lambda (x) (begin e1 e2))
-            vars, body = x[1], x[2:]
-            require(x, (isa(vars, list) and all(isa(v, Symbol) for v in vars))
-                    or isa(vars, Symbol), "illegal lambda argument list")
-            exp = body[0] if len(body) == 1 else [_begin] + body
-            return [_lambda, vars, expand(exp)]   
-        elif x[0] is _quasiquote:            # `x => expand_quasiquote(x)
-            require(x, len(x)==2)
-            return expand_quasiquote(x[1])
-        elif isa(x[0], Symbol) and x[0] in macro_table:
-            return expand(macro_table[x[0]](*x[1:]), toplevel) # (m arg...) 
-        else:                                #        => macroexpand if m isa macro
-            return [y for y in map(expand, x)]            # (f arg...) => expand each
-    
-    
-    
-
     _quote, _if, _set, _define, _lambda, _begin, _definemacro, = map(Sym, 
     "quote   if   set!  define   lambda   begin   define-macro".split())
     
@@ -352,19 +375,9 @@ def make_interpreter(extra_funcs=None):
 
     _append, _cons, _let = map(Sym, "append cons let".split())
 
-    def expand_quasiquote(x):
-        """Expand `x => 'x; `,x => x; `(,@x y) => (append x y) """
-        if not is_pair(x):
-            return [_quote, x]
-        require(x, x[0] is not _unquotesplicing, "can't splice here")
-        if x[0] is _unquote:
-            require(x, len(x)==2)
-            return x[1]
-        elif is_pair(x[0]) and x[0][0] is _unquotesplicing:
-            require(x[0], len(x[0])==2)
-            return [_append, x[0][1], expand_quasiquote(x[1:])]
-        else:
-            return [_cons, expand_quasiquote(x[0]), expand_quasiquote(x[1:])]
+
+    def local_expand(expr):
+        return expand(expr, macro_table, symbol_table)
     
     def let(*args):
         args = list(args)
@@ -374,7 +387,7 @@ def make_interpreter(extra_funcs=None):
         require(x, all(isa(b, list) and len(b)==2 and isa(b[0], Symbol)
                        for b in bindings), "illegal binding list")
         vars, vals = zip(*bindings)
-        return [[_lambda, list(vars)]+ [y for y in map(expand, body)]] + [w for w in map(expand, vals)]
+        return [[_lambda, list(vars)]+ [y for y in map(local_expand, body)]] + [w for w in map(local_expand, vals)]
 
     macro_table = {_let:let} ## More macros can go here
 
@@ -382,7 +395,7 @@ def make_interpreter(extra_funcs=None):
         "Parse a program: read and expand/error-check it."
         # Backwards compatibility: given a str, convert it to an InPort
         if isinstance(inport, str): inport = InPort(io.StringIO(inport))
-        return expand(read(inport, quotes), toplevel=True)
+        return expand(read(inport, quotes), macro_table, symbol_table, toplevel=True)
 
     
     # generic_eval(parse("""(begin
