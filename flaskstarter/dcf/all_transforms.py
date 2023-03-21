38cb99d5fdc483f2e48b6a767eb5c19f26ea95a8
Paddy Mullen <paddy@paddymullen.com>
2023-03-21 10:07:51 -0400

    added safe_int conversion

--- flaskstarter/dcf/all_transforms.py
+++ flaskstarter/dcf/all_transforms.py
@@ -56,6 +56,24 @@ group_df = pd.DataFrame({'a':[10,20,30,40,50,60],
                    'b':[1,2,3,4,5,6],
                    'c':['q', 'q', 'q', 'q', 'w', 'w']})
 
+def safe_int(x):
+    try:
+        return int(x)
+    except:
+        return np.nan
+
+class SafeInt(Transform):
+    command_default = [s('safeint'), s('df'), "col"]
+    command_pattern = [None]
+
+    @staticmethod 
+    def transform(df, col):
+        df[col] = df[col].apply(safe_int)
+        return df
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        return "    df[%s] = df[%s].apply(safe_int)" % (col, col)
 
 
 class GroupBy(Transform):
@@ -88,9 +106,10 @@ class GroupBy(Transform):
     
 
 
+    
 
 command_defaults, command_patterns, dcf_transform, dcf_to_py_core = configure_dcf([
-    FillNA, DropCol, OneHot, GroupBy])
+    FillNA, DropCol, OneHot, GroupBy, SafeInt])
 #print(GroupBy.test_output)
 print(dcf_transform(GroupBy.test_sequence, GroupBy.test_df))
 
